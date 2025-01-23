4b043be07869535c8d1c677e23c581e83aba144a
Daniel Zhu <dzhu2409@gmail.com>
2025-01-23 14:04:25 -0500

    Upload some configs

--- src/features/people/people.service.ts
+++ src/features/people/people.service.ts
@@ -1,11 +1,14 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger } from '@nestjs/common'
+import { CreatePersonDto } from './dto/create-person.dto'
+import { UpdatePersonDto } from './dto/update-person.dto'
 import { PrismaService } from 'src/core/prisma/prisma.service'
 
 @Injectable()
 export class PeopleService {
   constructor(private prismaService: PrismaService) {}
 
-  create() {
+  create(createPersonDto: CreatePersonDto) {
+    Logger.log(createPersonDto)
     return 'This action adds a new person'
   }
 
@@ -17,7 +20,9 @@ export class PeopleService {
     return `This action returns a #${id} person`
   }
 
-  update(id: number) {
+  update(id: number, updatePersonDto: UpdatePersonDto) {
+    Logger.log(updatePersonDto)
+
     return `This action updates a #${id} person`
   }
 
